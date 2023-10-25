05ac6381ecb225a88131b5e3c65566434d24e857
Melissa Martinez <123403764+martinez415@users.noreply.github.com>
2023-10-25 13:25:21 -0500

    Update DateClass.cpp

--- DateFormatApp/DateClass.cpp
+++ DateFormatApp/DateClass.cpp
@@ -14,7 +14,7 @@ DateClass::DateClass(int m, int d, int y) {
 // Method to obtain string representation of Month
 string DateClass::MonthName() {
 	string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-	for (int x = 1; x < 12; x++) {
+	for (int x = 0; x < 12; x++) {
 		return months[Month - 1];
 	}
 }
